154c1a135575a7484cf2fd3537904dfa0131dc6b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-23 01:19:29 +0600

    third post done

--- src/Pages/PostDetails/PostDetails.jsx
+++ src/Pages/PostDetails/PostDetails.jsx
@@ -9,12 +9,15 @@ import job from '../../assets/images/💼️ Job.png'
 import { GrLocation } from 'react-icons/gr';
 import { MdModeEdit } from 'react-icons/md';
 import { AiOutlineEye } from 'react-icons/ai';
+import { RiCalendarEventFill } from 'react-icons/ri';
 import dots from '../../assets/images/dots.png'
 import one from '../../assets/images/1.png'
 import two from '../../assets/images/2.png'
 import three from '../../assets/images/3.png'
 import four from '../../assets/images/4.png'
+import bag from '../../assets/images/bag.png'
 import share from '../../assets/images/share.png'
+import './PostDetails.css'
 
 const PostDetails = () => {
     return (
@@ -60,7 +63,7 @@ const PostDetails = () => {
                     <div className='d-flex justify-content-between align-items-center mx-3 mb-3'>
                         <div className='d-flex justify-content-center align-items-center gap-2'>
                             <img src={two} alt="" />
-                            <h6 className='fw-bold'>Sarthak Kamra</h6>
+                            <h6 className='fw-bold'>Sarah West</h6>
                         </div>
                         <div className='d-flex align-items-center gap-5'>
                             <div>
@@ -72,7 +75,52 @@ const PostDetails = () => {
                     </div>
                 </div>
 
+                <div className='border mb-3'>
+                    <img src={image3} alt="" />
+                    <img className='mt-3 ms-3' src={meetup} alt="" />
+                    <div className='d-flex justify-content-between mx-3 mt-3'>
+                        <h4>Finance & Investment Elite Social Mixer@Lujiazui</h4>
+                        <div>
+                            <img style={{ height: '7px' }} src={dots} alt="" />
+                        </div>
+                    </div>
+
+                    <div className='d-flex mt-1 mb-3 mx-3 gap-5'>
+                        <div className='d-flex align-items-center gap-1 '>
+                            <RiCalendarEventFill className='d-block' style={{ height: '20px' }}></RiCalendarEventFill>
+                            <span className='fw-medium'>Fri, 12 Oct, 2018</span>
+                        </div>
+                        <div className='d-flex align-items-center gap-1'>
+                            <div className=''>
+                                <GrLocation className='d-block' style={{ height: '20px' }}></GrLocation>
+                            </div>
+                            <span className='fw-medium'>Ahmedabad, India</span>
+                        </div>
+                    </div>
+
+                    <div className='d-grid gap-2 mx-3'>
+                        <button className="btn btn-outline-secondary  mb-3 text-danger btn-hover" type="button">Visit Website</button>
+                    </div>
+
+                    <div className='d-flex justify-content-between align-items-center mx-3 mb-3'>
+                        <div className='d-flex justify-content-center align-items-center gap-2'>
+                            <img src={three} alt="" />
+                            <h6 className='fw-bold'>Ronal Jones</h6>
+                        </div>
+                        <div className='d-flex align-items-center gap-5'>
+                            <div>
+                                <AiOutlineEye></AiOutlineEye>
+                                <span className='ms-2'>1.4k views</span>
+                            </div>
+                            <img className='px-3 py-2' style={{ height: '34px', backgroundColor: '#e5e5e5', borderRadius: '4px' }} src={share} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+
+
 
             <div style={{ marginTop: '50px' }}>
                 <div className="mb-3 row position-relative">
